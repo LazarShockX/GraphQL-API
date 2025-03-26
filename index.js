a16991cd5e510de480ca0e8d612cf9401f58707c
LazarShockX <donaldbr12@gmail.com>
2025-03-26 13:38:37 +0100

    Added Resolver functions

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs } from './typeDefs';
+import { typeDefs } from './schema.js';
+import db from './_db.js';
+
+const resolvers = {
+    Query: {
+        reviews: () => db.reviews,
+        games: () => db.games,
+        authors: () => db.authors
+    }
+}
 
 const server = new ApolloServer({
-    typeDefs
+    typeDefs,
+    resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
