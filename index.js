caff756f015eaca77751d090387c703e31d80b75
LazarShockX <donaldbr12@gmail.com>
2025-03-26 13:03:13 +0100

    Added Schema and Types

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { ApolloServer } from 'apollo-server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './typeDefs';
 
 const server = new ApolloServer({
-
+    typeDefs
 });
 
 const { url } = await startStandaloneServer(server, {
